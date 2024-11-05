5f6a51eaf0634c172daef6d4f4c8d862f680d67e
Adrian <28205593+nipadrian@users.noreply.github.com>
2024-11-04 21:19:14 -0600

    Add files via upload

--- day-38/main.py
+++ day-38/main.py
@@ -1,10 +1,19 @@
 import requests
+from datetime import datetime
+import os
 
-NUTRITION_ID = "1e49aa58"
-NUTRITION_KEY = "ba8228bb02ed6c2335642607c693b62c"
+
+NUTRITION_ID = ""
+NUTRITION_KEY = ""
+
+# os.environ["NUTRITION_ID"] = "1e49aa58"
+# os.environ["NUTRITION_KEY"] = NUTRITION_KEY
 
 host_domain = "https://trackapi.nutritionix.com"
 endpoint = "/v2/natural/exercise"
+
+# os.environ["host_domain"] = host_domain
+# os.environ["endpoint"] = endpoint
 
 headers = {
     "x-app-id": NUTRITION_ID,
@@ -18,3 +27,32 @@ params = {
 #print (query)
 response = requests.post(url = host_domain + endpoint, json = params, headers = headers)
 print(response.text)
+
+now = datetime.now()
+date = now.strftime("%m/%d/%Y")
+time = now.strftime("%H:%M:%S")
+exercise = (response.json()["exercises"][0]["name"])
+duration = (response.json()["exercises"][0]["duration_min"])
+calories = (response.json()["exercises"][0]["nf_calories"])
+
+
+sheety_domain = ""
+
+sheety_key = ""
+
+sheety_headers = {
+    "Authorization": sheety_key
+}
+
+sheety_input = {
+    "workout" : {
+        "date": date,
+        "time": time,
+        "exercise": exercise,
+        "duration": duration,
+        "calories": calories
+    }
+}
+
+sheety_post = requests.post(url = sheety_domain, json = sheety_input, headers = sheety_headers)
+print(sheety_post.text)
