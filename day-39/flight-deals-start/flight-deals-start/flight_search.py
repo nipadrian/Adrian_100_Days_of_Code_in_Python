69cad5a532359ed38a67c078c082c85689543ce5
Adrian <28205593+nipadrian@users.noreply.github.com>
2024-11-07 20:05:25 -0600

    Add files via upload

---
+++ day-39/flight-deals-start/flight-deals-start/flight_search.py
@@ -0,0 +1,7 @@
+class FlightSearch:
+    #This class is responsible for talking to the Flight Search API.
+
+    def get_destination_code(self,city_name):
+        code = "Test"
+        return code
+
