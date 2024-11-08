8e92e8d5b68f7082c1caf93fa67831b31cf11547
Adrian <28205593+nipadrian@users.noreply.github.com>
2024-11-07 20:06:01 -0600

    Delete day-39/flight-deals-start/flight-deals-start directory

--- day-39/flight-deals-start/flight-deals-start/flight_search.py
+++
@@ -1,7 +0,0 @@
-class FlightSearch:
-    #This class is responsible for talking to the Flight Search API.
-
-    def get_destination_code(self,city_name):
-        code = "Test"
-        return code
-
