acb28575eeab7998ad00ef74e32b9fdde8da92dd
Adrian <28205593+nipadrian@users.noreply.github.com>
2024-12-02 18:08:07 -0600

    Add files via upload

--- day-45/Starting+Code+-+100+movies+to+watch+start/Starting Code - 100 movies to watch start/main.py
+++ day-45/Starting+Code+-+100+movies+to+watch+start/Starting Code - 100 movies to watch start/main.py
@@ -5,4 +5,25 @@ URL = "https://web.archive.org/web/20200518073855/https://www.empireonline.com/m
 
 # Write your code below this line 👇
 
+response = requests.get(URL)
+website_html = response.text
+
+soup = BeautifulSoup(website_html, "html.parser")
+#print(soup)
+
+all_movies = soup.find_all(name = "h3", class_ = "title")
+print(all_movies)
+
+
+# for movie in all_movies:
+#     text = movie.getText()
+#     print(text)
+
+movie_titles = [movie.getText() for movie in all_movies]
+movies = (movie_titles[::-1])
+print(movies)
+
+with open("movies.txt",mode = "w") as file:
+    for movie in movies:
+        file.write(f"{movie}\n")
 
