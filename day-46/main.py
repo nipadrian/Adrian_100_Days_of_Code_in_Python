4d376817f5a82d4fa19e56f5dbadcede28590bfb
Adrian <28205593+nipadrian@users.noreply.github.com>
2024-12-04 22:49:53 -0600

    Add files via upload

--- day-46/main.py
+++ day-46/main.py
@@ -4,14 +4,15 @@ import requests
 year = input("Which year do you want to travel to? Type the date in this format YYYY-MM-DD:")
 
 #url = f"https://www.billboard.com/charts/hot-100/{year}"
-url = f"https://www.billboard.com/charts/hot-100/2000-04-27"
-header = {}
+#url = f"https://www.billboard.com/charts/hot-100/2000-04-27"
+header = {"User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:131.0) Gecko/20100101 Firefox/131.0"}
+url = "https://www.billboard.com/charts/hot-100/" + year
 
-response = requests.get(url)
+response = requests.get(url=url, headers = header)
 
 billboard_web_page = response.text
 
 soup = BeautifulSoup(billboard_web_page, "html.parser")
-print(soup)
-# song_names = soup.find_all(id = "title-of-a-story", class_ = "c-title")
-# print(song_names)
+song_names_spans = soup.select("li ul li h3")
+song_names = [song.getText().strip() for song in song_names_spans]
+print(song_names)
