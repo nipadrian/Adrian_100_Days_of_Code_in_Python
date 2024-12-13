e969296aad5a1ab88dca8747e7356f435de09d84
Adrian <28205593+nipadrian@users.noreply.github.com>
2024-12-13 14:54:53 -0600

    Add files via upload

--- day-53/main.py
+++ day-53/main.py
@@ -1,5 +1,11 @@
 from bs4 import BeautifulSoup
 import requests
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.support.ui import Select
 
 response = requests.get("https://appbrewery.github.io/Zillow-Clone/")
 
@@ -28,4 +34,39 @@ all_addresses = soup.find_all(name = "address")
 #zillow_addresses = [address.getText().split("\n")[1].replace(" ","") for address in all_addresses]
 zillow_addresses = [address.getText().replace("|"," ").strip() for address in all_addresses]
 
-print(zillow_addresses)
+#print(zillow_addresses)
+
+
+####### FILL IN FORM #######
+
+chrome_options = webdriver.ChromeOptions()
+chrome_options.add_experimental_option("detach",True)
+
+driver = webdriver.Chrome(options = chrome_options)
+driver.get(google_list)
+
+
+for n in range(len(zillow_prices)):
+    address_fill = "//*[@id='mG61Hd']/div[2]/div/div[2]/div[1]/div/div/div[2]/div/div[1]/div/div[1]/input"
+    address_element = WebDriverWait(driver, 10).until(
+        EC.element_to_be_clickable((By.XPATH, address_fill))
+    )
+
+    address_element.send_keys(zillow_addresses[n])
+
+    price_fill = driver.find_element(By.XPATH,
+                                     "//*[@id='mG61Hd']/div[2]/div/div[2]/div[2]/div/div/div[2]/div/div[1]/div/div[1]/input")
+    price_fill.send_keys(zillow_prices[n])
+
+    link_fill = driver.find_element(By.XPATH,
+                                    "//*[@id='mG61Hd']/div[2]/div/div[2]/div[3]/div/div/div[2]/div/div[1]/div/div[1]/input")
+    link_fill.send_keys(zillow_links[n])
+
+    submit = driver.find_element(By.XPATH, "//*[@id='mG61Hd']/div[2]/div/div[3]/div[1]/div[1]/div/span/span")
+    submit.click()
+
+    next = "/html/body/div[1]/div[2]/div[1]/div/div[4]/a"
+    next_response = WebDriverWait(driver, 10).until(
+        EC.element_to_be_clickable((By.XPATH, next))
+    )
+    next_response.click()
