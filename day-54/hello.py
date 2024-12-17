a18413dcdc25d956d9fb8e1d1ba2103c8672a158
Adrian <28205593+nipadrian@users.noreply.github.com>
2024-12-17 12:54:19 -0600

    Add files via upload

--- day-54/hello.py
+++ day-54/hello.py
@@ -1,8 +1,36 @@
 from flask import Flask
 app = Flask(__name__)
+#
+# @app.route('/')
+# def hello_world():
+#     return 'Hello, World!'
+# 
+# #hello_world()
+#
+# if __name__ == "__main__":
+#     app.run()
 
-@app.route('/')
-def hello_world():
-    return 'Hello, World!'
 
-hello_world()
+### Python Decorator Function
+import time
+
+def delay_decorator(function):
+    def wrapper_function():
+        time.sleep(2)
+        function()
+
+    return wrapper_function
+
+@delay_decorator
+def say_hello():
+    print("Hello")
+
+@delay_decorator
+def say_bye():
+    print("Bye")
+
+
+def say_greeting():
+    print("How are you?")
+
+say_bye()
