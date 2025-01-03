1246f6701012efcee2ad364ce1cd4099d8260f42
Adrian <28205593+nipadrian@users.noreply.github.com>
2025-01-02 18:00:14 -0600

    Add files via upload

--- day-56/server.py
+++ day-56/server.py
@@ -1,11 +1,12 @@
 from flask import Flask, render_template
 app = Flask(__name__)
 
-
+# chrome developer -> console -> document.body.contentEditable=true
+# need to save website with changes and add it back into code for updates
 
 @app.route('/')
 def hello_world():
-    return render_template("angela.html")
+    return render_template("index.html")
 
 if __name__ == "__main__":
     app.run(debug=True)
