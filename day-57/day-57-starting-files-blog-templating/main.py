2ecc82a0e924cc0f1c42873d4b180d7209685cd2
Adrian <28205593+nipadrian@users.noreply.github.com>
2025-01-16 17:07:29 -0600

    Add files via upload

--- day-57/day-57-starting-files-blog-templating/main.py
+++ day-57/day-57-starting-files-blog-templating/main.py
@@ -1,16 +1,26 @@
 from flask import Flask, render_template
 import requests
+from post import Post
 
+posts = requests.get("https://api.npoint.io/c790b4d5cab58020d391").json()
+post_objects = []
+for post in posts:
+    post_obj = Post(post["id"], post["title"], post["subtitle"], post["body"])
+    post_objects.append(post_obj)
 
 app = Flask(__name__)
 
 @app.route('/')
 def home():
-    blog_url = "https://api.npoint.io/c790b4d5cab58020d391"
-    response = requests.get(blog_url)
-    all_posts = response.json()
-    return render_template("index.html", posts = all_posts)
+    return render_template("index.html", posts = post_objects)
 
+@app.route("/post/<int:index>")
+def show_post(index):
+    requested_post = None
+    for blog_post in post_objects:
+        if blog_post.id == index:
+            requested_post = blog_post
+    return render_template("post.html", post=requested_post)
 
 if __name__ == "__main__":
     app.run(debug=True)
