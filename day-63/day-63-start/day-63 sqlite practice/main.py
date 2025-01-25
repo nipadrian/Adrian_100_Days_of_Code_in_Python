6e54db7d04f8c621d214338c603d7a911442a96f
Adrian <28205593+nipadrian@users.noreply.github.com>
2025-01-24 23:40:50 -0600

    Add files via upload

--- day-63/day-63-start/day-63 sqlite practice/main.py
+++ day-63/day-63-start/day-63 sqlite practice/main.py
@@ -14,3 +14,35 @@ from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from sqlalchemy.orm import DeclarativeBase, Mapped, mapped_column
 from sqlalchemy import Integer, String, Float
+
+app = Flask(__name__)
+
+# Create Database
+class Base(DeclarativeBase):
+    pass
+
+app.config['SQLALCHEMY_DATABASE_URI'] = "sqlite:///new-books-collection.db"
+
+# Create extension
+db = SQLAlchemy(model_class=Base)
+
+# Initialise app with extension
+db.init_app(app)
+
+
+# Create table
+class Book(db.Model):
+    id: Mapped[int] = mapped_column(primary_key = True)
+    title: Mapped[str] = mapped_column(String(250),unique=True, nullable=False)
+    author: Mapped[str] = mapped_column(String(250),nullable=False)
+    rating: Mapped[int] = mapped_column(Float,nullable=False)
+
+# Create table schema in database. Requires application context.
+with app.app_context():
+    db.create_all()
+
+# Create record
+with app.app_context():
+    new_book = Book(id=1, title = "Harry Potter", author = "J. K. Rowling", rating = 9.3)
+    db.session.add(new_book)
+    db.session.commit()
